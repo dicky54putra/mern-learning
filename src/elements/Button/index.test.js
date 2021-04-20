cd19375f3519b621d4779b21ff0fc112c7eb63c2
Dicky Saputra <saputradicky705@gmail.com>
2021-04-20 22:38:17 +0700

    componen button

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {render} from '@testing-library/react';
+import Button from './index';
+import { BrowserRouter as Router} from 'react-router-dom'
+
+
+test('should not allow clickbutton if disabled is present', () => {
+    const {container} = render(<Button isDisabled></Button>)
+
+    expect(container.querySelector('span.disabled')).toBeInTheDocument()
+})
+
+test('should render loading/sinner', () => {
+    const {container, getByText} = render(<Button isLoading></Button>)
+
+    expect(getByText(/loading/i)).toBeInTheDocument()
+    expect(container.querySelector('span')).toBeInTheDocument()
+})
+
+test('should render <a> tag ', () => {
+    const {container} = render(<Button type="link" isExternal></Button>)
+
+    expect(container.querySelector('a')).toBeInTheDocument()
+})
+
+test('should render <Link> component ', () => {
+    const {container} = render(
+        <Router>
+            <Button href="" type="link"></Button>
+        </Router>
+    )
+
+    expect(container.querySelector('a')).toBeInTheDocument()
+})
+
